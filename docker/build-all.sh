2cefc04030c1cce441fa6dcfae54ec333af3547d
clwang23 <clwang23@debian11.localdomain>
2023-09-20 14:56:57 +0800

    modify build_all.sh

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -32,35 +32,36 @@ done
 
 
 # older method
-if [[ -z ${1} ]]; then
-    builds=( "base" "python" "java" "nodejs" )
-else
-    builds=( "$@" )
-fi
+#+ Commented them 20/09/23+
+# if [[ -z ${1} ]]; then
+#     builds=( "base" "python" "java" "nodejs" )
+# else
+#     builds=( "$@" )
+# fi
 
-for b in "${builds[@]}"; do
-    dirname=${b/tests\//""}
-    docker_img_name="witcher/${b}"
-    puppeteer_img_name="puppeteer1337/${b}"
-    docker_img_name=${docker_img_name/tests\//""}
-    docker_img_name=${docker_img_name/\/base/"/basebuildrun"}
-    dockerfile_path="${DIR}/${b}.Dockerfile"
-    if [[ ! -f ${dockerfile_path} ]]; then
-        dockerfile_path=${DIR}/../${b}/Dockerfile
-    fi
-    if [[ $docker_img_name == "witcher" ]]; then
-        puppeteer_img_name="puppeteer1337/witcher-base"
-    fi
-    context="${DIR}/../${b}"
+# for b in "${builds[@]}"; do
+#     dirname=${b/tests\//""}
+#     docker_img_name="witcher/${b}"
+#     puppeteer_img_name="puppeteer1337/${b}"
+#     docker_img_name=${docker_img_name/tests\//""}
+#     docker_img_name=${docker_img_name/\/base/"/basebuildrun"}
+#     dockerfile_path="${DIR}/${b}.Dockerfile"
+#     if [[ ! -f ${dockerfile_path} ]]; then
+#         dockerfile_path=${DIR}/../${b}/Dockerfile
+#     fi
+#     if [[ $docker_img_name == "witcher" ]]; then
+#         puppeteer_img_name="puppeteer1337/witcher-base"
+#     fi
+#     context="${DIR}/../${b}"
 
-    printf "\033[34mBuilding ::> ${docker_img_name} using Dockerfile ${dockerfile_path} and context ${context}\033[0m\n"
-    echo 'docker build -t '${docker_img_name}' -f "'${dockerfile_path}'" "'${context}'"'
+#     printf "\033[34mBuilding ::> ${docker_img_name} using Dockerfile ${dockerfile_path} and context ${context}\033[0m\n"
+#     echo 'docker build -t '${docker_img_name}' -f "'${dockerfile_path}'" "'${context}'"'
 
-    if docker build -t ${docker_img_name} -f "${dockerfile_path}" "${context}"; then
-        docker tag ${docker_img_name} ${puppeteer_img_name}
-        printf "\033[32mSucessfully built ${puppeteer_img_name} using Dockerfile ${dockerfile_path} and context ${context}\033[0m\n"
-    else
-        printf "\033[31mFailed to build ${docker_img_name} using Dockerfile ${dockerfile_path} and context ${context}\033[0m\n"
-        exit 191
-    fi
-done
+#     if docker build -t ${docker_img_name} -f "${dockerfile_path}" "${context}"; then
+#         docker tag ${docker_img_name} ${puppeteer_img_name}
+#         printf "\033[32mSucessfully built ${puppeteer_img_name} using Dockerfile ${dockerfile_path} and context ${context}\033[0m\n"
+#     else
+#         printf "\033[31mFailed to build ${docker_img_name} using Dockerfile ${dockerfile_path} and context ${context}\033[0m\n"
+#         exit 191
+#     fi
+# done
